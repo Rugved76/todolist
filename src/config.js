3ea1a5e4246b90a84d28be8db78b5357522e03f0
Rugved76 <rugvedwagh02@gmail.com>
2023-06-01 22:31:28 +0530

    as

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/fulltodo"
+    // url: "mongodb://localhost:27017/fulltodo"
+    url: "mongodb+srv://rugved:1234@clusterx.mc03un9.mongodb.net/?retryWrites=true&w=majority"
 };
